0798ac0d6863a75698ab4dc8db713dbdac3fc60a
Melvin Fallas <melfallas@yahoo.com>
2021-05-09 23:48:25 -0600

    E. Resource server configurations.

--- src/main/java/com/mb/base/idp/config/oauth/ResourceServerConfig.java
+++ src/main/java/com/mb/base/idp/config/oauth/ResourceServerConfig.java
@@ -1,14 +1,23 @@
 package com.mb.base.idp.config.oauth;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	private final String SINGING_KEY = "secretKey1";
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
@@ -17,5 +26,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.anyRequest().authenticated()
 		;
 	}
+	
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    	resources.tokenServices(tokenService());
+    }
+    
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenService() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SINGING_KEY);
+        //converter.setVerifierKey(JwtConfig.LLAVE_SECRETA);
+        return converter;
+    }
 	
 }
